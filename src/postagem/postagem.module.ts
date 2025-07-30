dcb0516e00d1135ee7eaa12ad5c1d0186000bb67
Grazielle Gualter <grazi.gualter@gmail.com>
2025-07-29 22:58:13 -0300

    Método classe Postagem Controller e Service

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Postagem } from './entities/postagem.entity';
+import { PostagemController } from './controllers/postagem.controller';
+import { PostagemService } from './services/postagem.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Postagem])],
-  controllers: [],
-  providers: [],
+  controllers: [PostagemController],
+  providers: [PostagemService],
   exports: [],
 })
 export class PostagemModule{}
